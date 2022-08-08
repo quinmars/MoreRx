9328e976e431ba6c9c823e8bc4f9372588287431
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2022-08-09 00:51:37 +0200

    Add SmallestBy operators

---
+++ MoreRx/Operators/SmallestBy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+
+namespace MoreRx
+{
+    public static partial class MoreObservable
+    {
+        public static IOrderedObservable<TSource> SmallestBy<TSource, TSelect>(this IObservable<TSource> source, Func<TSource, TSelect> selector, int count)
+        {
+            return new CappedOrderedObservable<TSource, TSelect>(source, selector, null, descending: true, count, DefaultSortScheduler);
+        }
+
+        public static IOrderedObservable<TSource> SmallestBy<TSource, TSelect>(this IObservable<TSource> source, Func<TSource, TSelect> selector, int count, IScheduler scheduler)
+        {
+            return new CappedOrderedObservable<TSource, TSelect>(source, selector, null, descending: true, count, scheduler);
+        }
+
+        public static IOrderedObservable<TSource> SmallestBy<TSource, TSelect>(this IObservable<TSource> source, Func<TSource, TSelect> selector, int count, IComparer<TSelect>? comparer)
+        {
+            return new CappedOrderedObservable<TSource, TSelect>(source, selector, comparer, descending: true, count, DefaultSortScheduler);
+        }
+
+        public static IOrderedObservable<TSource> SmallestBy<TSource, TSelect>(this IObservable<TSource> source, Func<TSource, TSelect> selector, int count, IComparer<TSelect>? comparer, IScheduler scheduler)
+        {
+            return new CappedOrderedObservable<TSource, TSelect>(source, selector, comparer, descending: true, count, scheduler);
+        }
+    }
+}
