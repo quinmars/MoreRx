a8b543394deed5e6add0e86188863f68aed4f7e0
Peter Wehrfritz <peter.wehrfritz@mailbox.org>
2022-08-28 22:27:18 +0200

    Add benchmark for OrderBy

---
+++ MoreRx.Benchmark/Operators/OrderByBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace MoreRx.Benchmark
+{
+    public class OrderByBenchmark
+    {
+        private static readonly int[] s_steps = new int[] { 1, 10, 100, 1_000 };
+
+        private int _store;
+
+        [Benchmark]
+        [ArgumentsSource(nameof(RandomNumbers))]
+        public void Sort(int[] values)
+        {
+            values
+                .ToObservable()
+                .OrderBy(x => x % 30)
+                .ThenBy(x => x)
+                .Subscribe(v => Volatile.Write(ref _store, v));
+        }
+
+        public static IEnumerable<int[]> RandomNumbers()
+        {
+            var rnd = new Random(0);
+            return s_steps
+                .Select(n => Enumerable.Range(0, n).Select(_ => rnd.Next()).ToArray())
+                .ToArray();
+        }
+    }
+}
---
+++ MoreRx.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+var config = DefaultConfig
+    .Instance
+    .AddJob(Job.Default.WithCustomBuildConfiguration("Baseline"))
+    .AddJob(Job.Default.WithCustomBuildConfiguration("Current"));
+
+BenchmarkSwitcher
+    .FromAssemblies(new[] { typeof(Program).Assembly })
+    .Run(args, config);
